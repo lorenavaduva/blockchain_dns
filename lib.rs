5ae6476c29c1a23babb8707f29b5c092e4571bac
lori <lorena.vaduva413@gmail.com>
2023-06-10 22:31:52 +0300

    latest

--- lib.rs
+++ lib.rs
@@ -21,7 +21,7 @@ mod dns {
     #[ink(storage)]
     pub struct Dns {
         domain_map: Mapping<Vec<u8>,DomainData>,
-        //domain_map: Mapping<u8,u8>,
+        // subdomains: Mapping<(AccountId,SubdomainData), ()>
     }
     #[derive(scale::Encode, scale::Decode)]
     #[cfg_attr(
@@ -31,13 +31,53 @@ mod dns {
     pub struct DomainData {
         owner: AccountId,
         ip_address: Vec<u8>,
+        subdomains: Vec<SubdomainData>,
+        
+    }
+    #[derive(scale::Encode, scale::Decode)]
+    #[cfg_attr(
+        feature = "std",
+        derive(scale_info::TypeInfo, ink::storage::traits::StorageLayout)
+    )]
+    pub struct SubdomainData {
+        subdomain: Vec<u8>,
+        //ip_address: [u8; 4],//modify
+        ip_address: Vec<u8>    
     }
 
     impl Dns {
 
-
+        //create defaults for domain and domaindata ?
         #[ink(constructor)]
         pub fn new() -> Self {
+            // let mut int_domain_map = Mapping::new();
+            // let mut int_subdomain_map = Mapping::new();
+            // let mut vec = Vec::<u8>::new();
+            // vec.push(0u8);
+
+            // let subdomain_data = SubdomainData {
+            //     owner: Self::zero_address(),
+            //     ip_address:vec.clone(),
+            // };
+            // int_subdomain_map.insert(vec, &subdomain_data);
+            // let domain_data = DomainData {
+            //     owner: Self::zero_address(),
+            //     ip_address: vec.clone(),
+            //     subdomains: int_subdomain_map
+            // };
+
+            // int_domain_map.insert(vec, &domain_data);
+
+            Self {
+                domain_map: Self::default_domain()
+            }
+        }
+
+        fn zero_address() -> AccountId {
+            [0u8; 32].into()
+        }
+
+        fn default_domain() -> Mapping<Vec<u8>,DomainData> {
             let mut int_domain_map = Mapping::new();
             let mut vec = Vec::<u8>::new();
             vec.push(0u8);
@@ -45,17 +85,25 @@ mod dns {
             let domain_data = DomainData {
                 owner: Self::zero_address(),
                 ip_address: vec.clone(),
-                // subdomains: Default::default(),
+                subdomains: Self::default_subdomain()
             };
             int_domain_map.insert(vec, &domain_data);
-
-            Self {
-                domain_map: int_domain_map
-            }
+            return int_domain_map;
         }
 
-        fn zero_address() -> AccountId {
-            [0u8; 32].into()
+        fn default_subdomain() -> Mapping<(AccountId,SubdomainData), ()>{
+
+            let mut int_subdomain_map = Mapping::new();
+            let mut vec = Vec::<u8>::new();
+            vec.push(0u8);
+
+            let subdomain_data = SubdomainData {
+                owner: Self::zero_address(),
+                ip_address:vec.clone(),
+            };
+
+            int_subdomain_map.insert(vec, &subdomain_data);
+            return int_subdomain_map;
         }
 
         pub fn default(&self)   {
@@ -79,7 +127,7 @@ mod dns {
             let domain_data = DomainData {
                 owner: caller,
                 ip_address,
-                // subdomains: Default::default(),
+                subdomains: Default::default(),
             };
             self.domain_map.insert(domain, &domain_data);
             Ok(())

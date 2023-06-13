1b0abb056ec1a88e82741520b74521dafd7941e5
lori <lorena.vaduva413@gmail.com>
2023-06-13 03:03:36 +0300

    cleaned up added domain register/unregister tests and logging

--- lib.rs
+++ lib.rs
@@ -9,11 +9,11 @@ mod dns {
 
     #[derive(Debug, PartialEq, Eq, scale::Encode, scale::Decode,TypeInfo)]
     pub enum DnsError {
-        InvalidDomain,
+        // InvalidDomain,
         DomainAlreadyRegistered,
         DomainNotRegistered,
-        SubdomainAlreadyRegistered,
-        SubdomainNotRegistered,
+        // SubdomainAlreadyRegistered,
+        // SubdomainNotRegistered,
         NotAuthorized,
     }
 
@@ -32,10 +32,9 @@ mod dns {
         ip_address: Vec<u8>,  
     }
 
-    impl Dns {
+    impl Default for Dns {
 
-        #[ink(constructor)]
-        pub fn new() -> Self {
+        fn default() -> Self {
             let mut int_domain_map = Mapping::new();
             let mut vec = Vec::<u8>::new();
             vec.push(0u8);
@@ -45,35 +44,21 @@ mod dns {
                 ip_address: vec.clone(),
             };
             int_domain_map.insert(vec, &domain_data);
-
-            Self {
-                // domain_map: Self::default_domain()
+            Self{
                 domain_map: int_domain_map
             }
+        }
+    }
+    impl Dns {
+
+        #[ink(constructor)]
+        pub fn new() -> Self {
+            Default::default()
         }
 
         fn zero_address() -> AccountId {
             [0u8; 32].into()
         }
-
-        fn default_domain() -> Mapping<Vec<u8>,DomainData> {
-            let mut int_domain_map = Mapping::new();
-            let mut vec = Vec::<u8>::new();
-            vec.push(0u8);
-
-            let domain_data = DomainData {
-                owner: Self::zero_address(),
-                ip_address: vec.clone(),
-            };
-            int_domain_map.insert(vec, &domain_data);
-            return int_domain_map;
-        }
- 
-
-        pub fn default(&self)   {
-            
-        }
-
 
         #[ink(message)]
         pub fn register_domain(
@@ -81,15 +66,19 @@ mod dns {
             domain: Vec<u8>,
             ip_address: Vec<u8>,
         ) -> Result<(), DnsError> {
+            ink_env::debug_println!("pula");
             let caller = self.env().caller();
             if self.domain_map.contains(&domain) {
+                ink_env::debug_println!("Domain is already registered");
                 return Err(DnsError::DomainAlreadyRegistered);
             }
             let domain_data = DomainData {
                 owner: caller,
                 ip_address,
             };
+            let domain_for_debug = domain.clone(); 
             self.domain_map.insert(domain, &domain_data);
+            ink_env::debug_println!("Domain was successfully registered: {:?}", core::str::from_utf8(&domain_for_debug));
             Ok(())
         }
 
@@ -114,4 +103,54 @@ mod dns {
                 .map(|data| data.ip_address.clone())
         }
     }
+
+
+
+    #[cfg(test)]
+    mod tests {
+        use super::*;
+
+        fn default_accounts(
+        ) -> ink::env::test::DefaultAccounts<ink::env::DefaultEnvironment> {
+            ink::env::test::default_accounts::<Environment>()
+        }
+
+        fn set_next_caller(caller: AccountId) {
+            ink::env::test::set_caller::<Environment>(caller);
+        }
+
+        #[ink::test]
+        fn test_register_domain() {
+            let mut contract = Dns::new();
+            let domain = b"example.org".to_vec();
+            let ip_address = b"192.168.0.1".to_vec();
+            let non_existent_domain = b"nonexistent.org".to_vec();
+            assert_eq!(contract.register_domain(domain.clone(), ip_address.clone()), Ok(()));
+            assert_eq!(contract.resolve_domain(non_existent_domain), None);
+            assert_eq!(contract.register_domain(domain.clone(), ip_address.clone()), Err(DnsError::DomainAlreadyRegistered));
+        }
+
+        #[ink::test]
+        fn test_unregister_domain() {
+            let mut contract = Dns::new();
+            let domain = b"example.org".to_vec();
+            let ip_address = b"192.168.0.1".to_vec();
+            let accounts = default_accounts();
+            let non_existent_domain = b"nonexistent.org".to_vec();
+        
+            //Domain not registered case
+            assert_eq!(contract.unregister_domain(non_existent_domain.clone()), Err(DnsError::DomainNotRegistered));
+            //Unregister a domain when not authorized
+            set_next_caller(accounts.alice);
+            assert_eq!(contract.register_domain(domain.clone(), ip_address.clone()), Ok(()));
+            set_next_caller(accounts.bob);
+            assert_eq!(contract.unregister_domain(domain.clone()), Err(DnsError::NotAuthorized));
+            //Unregister the domain by the owner.
+            set_next_caller(accounts.alice);
+            assert_eq!(contract.unregister_domain(domain.clone()), Ok(()));
+            assert_eq!(contract.resolve_domain(domain.clone()), None);
+        }
+        
+    }
 }
+
